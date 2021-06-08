c1aa0fce0847507e7ccbc59876c5b1f667246052
wickedbrat <harshsrivastav123@gmail.com>
2021-06-09 00:44:01 +0530

    final

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,67 @@ export default function Home() {
 			<Head>
 				<title>WickedBrat</title>
 				<link rel="icon" href="/logo_white.png" />
+				<meta name="generator" content="Hugo 0.74.3" />
+				<title>Siddhant Srivastav</title>
+
+				<meta charset="UTF-8" />
+				<meta name="language" content="en" />
+				<meta
+					name="description"
+					content="A place on the internet just for knowing Siddhant better! Learn more about me here..."
+				/>
+				<meta
+					name="keywords"
+					content="wicked , brat , wickedbrat , Siddhant Srivastav , Gojek , Engineer , Product , Tech , Personal , Website"
+				/>
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+				<meta name="twitter:card" content="summary" />
+				<meta name="twitter:title" content="" />
+				<meta
+					name="twitter:description"
+					content="A place on the internet just for knowing Siddhant better! Learn more about me here..."
+				/>
+				<meta name="twitter:site" content="https://twitter.com/siddhantsme" />
+				<meta name="twitter:creator" content="https://twitter.com/siddhantsme" />
+				<link
+					rel="alternate"
+					type="application/rss+xml+xml"
+					href="https://wickedbrat.com/index.xml"
+					title="Siddhant Srivastav"
+				/>
+				<script
+					type="application/ld+json"
+					dangerouslySetInnerHTML={{
+						__html: `{ 
+								"@context": "http://schema.org", 
+								"@type": "WebSite", 
+								"url": "https:\/\/wickedbrat.com\/",
+								"name": "",
+								"author": {
+									"@type": "Person",
+									"name": ""
+								},
+								"description": "A place on the internet just for knowing Siddhant better! Learn more about me here..."
+							}`,
+					}}
+				></script>
+
+				<script
+					type="application/javascript"
+					dangerouslySetInnerHTML={{
+						__html: `var doNotTrack = false;
+	if (!doNotTrack) {
+		window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+		ga('create', 'UA-130399233-1', 'auto');
+		
+		ga('send', 'pageview');
+	}`,
+					}}
+				></script>
+				<script async="" src="https://www.google-analytics.com/analytics.js"></script>
 			</Head>
 			<div className="flex flex-col items-center justify-center text-white" id="landing">
 				<main className="flex flex-col flex-wrap items-center justify-center flex-1 px-20 text-9xl">
