05b8352172dc32ce71949aed09b2e2c903967044
wickedbrat <harshsrivastav123@gmail.com>
2021-06-09 00:34:13 +0530

    review fixes

--- pages/_howdy.js
+++ pages/_howdy.js
@@ -1,9 +1,22 @@
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 export default function Howdy() {
 	function getText(text, show = false) {
 		return (
 			<div className="flex justify-center items-center sticky top-1/4">
 				<p className="text-6xl font-bold text-white bg-red w-full" style={{ height: "150vh" }}>
-					{text}
+					<p>
+						{text === "Sure! Ping me on Instagram" ? (
+							<p>
+								Sure! Ping me on {""}
+								<a className="w-10" href="https://www.instagram.com/siddhantisme/" target="_blank">
+									Instagram
+								</a>
+							</p>
+						) : (
+							text
+						)}
+					</p>
 					{show ? getSocial() : ""}
 				</p>
 			</div>
@@ -44,25 +57,43 @@ export default function Howdy() {
 
 	function getSocial() {
 		return (
-			<div className="flex w-1/3 mt-10 justify-between items-center">
-				<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
-					<img src="/img/medium.svg" />
-				</a>
-				<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
-					<img src="/img/email.svg" />
-				</a>
-				<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
-					<img src="/img/github.svg" />
-				</a>
-				<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
-					<img src="/img/facebook.svg" />
-				</a>
-				<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
-					<img src="/img/linkedin.svg" />
-				</a>
-				<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
-					<img src="/img/twitter.svg" />
-				</a>
+			<div className="flex flex-col w-full">
+				<div className="flex w-1/3 mt-10 justify-between items-center">
+					<a className="w-10" href="https://medium.com/@wickedbrat" target="_blank">
+						<img src="/img/medium.svg" />
+					</a>
+					<CopyToClipboard options={{ message: "Whoa!" }} text={"harshsrivastav123@gmail.com"}>
+						<div
+							onMouseEnter={() => {
+								document.getElementById("hover").style.display = "block";
+							}}
+							onMouseLeave={() => {
+								document.getElementById("hover").style.display = "none";
+							}}
+						>
+							<a className="w-10 cursor-pointer">
+								<img src="/img/email.svg" />
+							</a>
+						</div>
+					</CopyToClipboard>
+
+					<a className="w-10" href="https://www.instagram.com/siddhantisme/" target="_blank">
+						<img src="/img/instagram.svg" />
+					</a>
+					<a className="w-10" href="https://github.com/wickedbrat" target="_blank">
+						<img src="/img/github.svg" />
+					</a>
+					<a className="w-10" href="https://www.linkedin.com/in/wickedbrat/" target="_blank">
+						<img src="/img/linkedin.svg" />
+					</a>
+					<a className="w-10" href="https://twitter.com/siddhantsme" target="_blank">
+						<img src="/img/twitter.svg" />
+					</a>
+				</div>
+
+				<div class="relative mx-2 hidden" id="hover">
+					<div class=" text-white text-xs rounded py-1 px-4 right-0 bottom-full">Click to copy my email</div>
+				</div>
 			</div>
 		);
 	}
